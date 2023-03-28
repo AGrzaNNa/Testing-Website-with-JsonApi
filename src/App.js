6e5db40287c042b341d02110e7ccc97e88fef842
Sequin13 Waitinskyy <kubaczekajski@gmail.com>
2023-03-28 22:13:07 +0200

    Podstawowa funkcjonalność i testy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [posts, setPosts] = useState([]);
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSearchClick = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then((data) => {
+                const matchingPosts = data.filter((post) =>
+                    post.title.toLowerCase().startsWith(searchTerm.toLowerCase())
+                );
+                setPosts(matchingPosts);
+            });
+    };
+
+    const handleCommentClick = (postId) => {
+        const postIndex = posts.findIndex((post) => post.id === postId);
+        const post = posts[postIndex];
+        if (post.commentsShown) {
+            post.commentsShown = false;
+            setPosts([...posts]);
+        } else {
+            fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    post.commentsShown = true;
+                    post.comments = data;
+                    setPosts([...posts]);
+                });
+        }
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <body
+      className="App-body">
+
+      <header>
+          <h2 className="App-Logo">
+              ByteBusters
+          </h2>
+          <nav className="App-navi">
+              <a href="#">Options</a>
+              <button className="btnlogin">Login</button>
+          </nav>
       </header>
+    <div className="App">
+        <div>
+            <div>
+                <div>
+                <input type="text" name ="search-input" placeholder="search anything" value={searchTerm} onChange={handleSearchChange} >
+                </input>
+                <button onClick={handleSearchClick}>
+                    Search
+                </button>
+                </div>
+            </div>
+            <div id="search-results">
+                {posts.length === 0 && <p>No results found.</p>}
+                {posts.map((post) => (
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>Author: {post.userId}</p>
+                        <p>{post.body}</p>
+                        <button className="showcomments" onClick={() => handleCommentClick(post.id)}>
+                            {post.commentsShown ? 'Hide Comments' : 'Show Comments'}
+                        </button>
+                        {post.commentsShown && (
+                            <ul>
+                                {post.comments.map((comment) => (
+                                    <li key={comment.id}>
+                                        <p>Author: {comment.email}</p>
+                                        <p>{comment.body}</p>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </div>
     </div>
+  </body>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('is site contains logo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/ByteBusters/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('is site contains search-bar', () => {
+  render(<App />);
+  const searchBar = screen.getByRole('textbox', { name: '' });
+  expect(searchBar).toBeInTheDocument();
+});
+
+test('is search with phrase qui finding 7 posts', async () => {
+    render(<App />);
+
+    const searchInput = screen.getByRole('textbox', { name: '' });
+    fireEvent.change(searchInput, { target: { value: 'qui' } });
+
+    const searchButton = screen.getByRole('button', { name: 'Search' });
+    fireEvent.click(searchButton);
+    await waitFor(() => {
+      expect(screen.queryByText('No results found.')).toBeNull();
+    });
+
+    const matchingPosts = screen.getAllByRole('heading', { level: 2, name: /^qui/i });
+    expect(matchingPosts).toHaveLength(7);
+  });
+test('login button is clickable', () => {
+    render(<App />);
+
+    const loginButton = screen.getByRole('button', { name: 'Login' });
+    expect(loginButton).toBeInTheDocument();
+
 });
